a97dfa5052369053de166561711fb53aa6aba732
Bruno Dias <dias.h.bruno@gmail.com>
2026-05-11 21:19:42 -0300

    Update routes.

--- src/api.lisp
+++ src/api.lisp
@@ -74,22 +74,25 @@
 
 (defun build-app-routes ()
   (condition-handler #'development-condition-handler)
+
   (io.github.cl-sdk.wst.routing.dsl:build-webserver
-   `(io.github.cl-sdk.wst.routing.dsl:group
-     (io.github.cl-sdk.wst.routing.dsl:route :GET index "/" index-handler)
-     (io.github.cl-sdk.wst.routing.dsl:route :GET health "/health" health-handler)
-     (io.github.cl-sdk.wst.routing.dsl:resource
-      "/api/v1"
-      (io.github.cl-sdk.wst.routing.dsl:wrap
-       :before (,*parse-content-middleware*)
-       :route
-       (io.github.cl-sdk.wst.routing.dsl:resource "/links"
-						  (io.github.cl-sdk.wst.routing.dsl:route :POST create-short-url create-short-url-handler)
-						  (io.github.cl-sdk.wst.routing.dsl:route :GET list-short-urls list-short-urls-handler)
-						  (io.github.cl-sdk.wst.routing.dsl:route :GET inspect-short-url "/:code" inspect-short-url-handler)
-						  (io.github.cl-sdk.wst.routing.dsl:route :DELETE delete-short-url "/:code" delete-short-url-handler))))
-     (io.github.cl-sdk.wst.routing.dsl:route :GET redirect-short-url "/:code" redirect-short-url-handler)
-     (io.github.cl-sdk.wst.routing.dsl:any-route :GET not-found-handler))))
+   `(io.github.cl-sdk.wst.routing.dsl:wrap
+     :before (,*parse-content-middleware*)
+     :route
+     (io.github.cl-sdk.wst.routing.dsl:group
+      (io.github.cl-sdk.wst.routing.dsl:route :GET index "/" index-handler)
+      (io.github.cl-sdk.wst.routing.dsl:route :GET health "/health" health-handler)
+      (io.github.cl-sdk.wst.routing.dsl:resource
+       "/api/v1"
+       (io.github.cl-sdk.wst.routing.dsl:group
+	(io.github.cl-sdk.wst.routing.dsl:resource
+	 "/links"
+	 (io.github.cl-sdk.wst.routing.dsl:route :POST create-short-url create-short-url-handler)
+	 (io.github.cl-sdk.wst.routing.dsl:route :GET list-short-urls list-short-urls-handler)
+	 (io.github.cl-sdk.wst.routing.dsl:route :GET inspect-short-url "/:code" inspect-short-url-handler)
+	 (io.github.cl-sdk.wst.routing.dsl:route :DELETE delete-short-url "/:code" delete-short-url-handler))))
+      (io.github.cl-sdk.wst.routing.dsl:route :GET redirect-short-url "/:code" redirect-short-url-handler)
+      (io.github.cl-sdk.wst.routing.dsl:any-route :GET not-found-handler)))))
 
 (defun app (env)
   (let* ((request (request-from-woo-env env))
